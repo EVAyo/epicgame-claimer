7292f75ba411bb796cc1a9971e0a57be17d42bad
luminoleon <luminoleon@outlook.com>
2021-03-13 15:28:57 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Epicgames Claimer
+
+## 使用方法
+
+1. 安装Docker
+2. 运行命令 docker run -it -e TZ=Asia/Shanghai luminoleon/epicgames-claimer
+3. 按照提示输入邮箱、密码、两步验证代码
+4. 按Ctrl + P + Q切换至后台运行
--- claimer.py
+++ claimer.py
@@ -9,13 +9,14 @@ class epic_claimer:
     def __init__(self):
         loop = asyncio.get_event_loop()
         self.browser = loop.run_until_complete(
-            launch(options={"args": ["--no-sandbox"], "headless": False}, userDataDir="userdata")
+            launch(options={"args": ["--no-sandbox"],
+                            "headless": False}, userDataDir="userdata")
         )
         self.page = loop.run_until_complete(self.browser.pages())[0]
 
     def close(self):
         asyncio.get_event_loop().run_until_complete(self.browser.close())
-    
+
     def log(self, text):
         localtime = time.asctime(time.localtime(time.time()))
         print("[{}] {}".format(localtime, text))
@@ -29,11 +30,11 @@ class epic_claimer:
         await self.page.waitForSelector(selector)
         time.sleep(sleep_time)
         await self.page.click(selector)
-    
+
     async def await_get_text(self, selector, property="textContent"):
         await self.page.waitForSelector(selector)
         return await (await (await self.page.querySelector(selector)).getProperty(property)).jsonValue()
-    
+
     async def await_detect(self, selector, timeout=None):
         try:
             if timeout != None:
@@ -43,7 +44,7 @@ class epic_claimer:
             return True
         except:
             return False
-    
+
     async def await_try_click(self, selector, sleep_time=2):
         if await self.await_detect(selector):
             time.sleep(sleep_time)
@@ -77,44 +78,44 @@ class epic_claimer:
                         self.exit()
             self.log("Login successed.")
             self.log("Now you can leave by pressing Ctrl + P + Q.")
-    
+
     async def order(self, selector):
         if await self.await_detect("#purchase-app div.navigation-element.complete"):
             if "0.00" in (await self.await_get_text("#purchase-app div.price-row-container.total")):
                 if await self.await_try_click(selector):
                     await self.page.waitForSelector("div[class*=DownloadLogoAndTitle__header]")
                     self.log("Claim successed.")
-    
+
     async def claim(self):
         await self.page.goto("https://www.epicgames.com/store/zh-CN/free-games",
-            options={"timeout": 120000}
-        )
-        await self.page.waitForSelector("div[data-component=CustomDiscoverModules] > div:nth-child(2) " \
-            "div[data-component=CardGridDesktopBase]"
-        )
-        item_list = await self.page.querySelectorAll("div[data-component=CustomDiscoverModules] > " \
-            "div:nth-child(2) div[data-component=CardGridDesktopBase]"
-        )
+                             options={"timeout": 120000}
+                             )
+        await self.page.waitForSelector("div[data-component=CustomDiscoverModules] > div:nth-child(2) "
+                                        "div[data-component=CardGridDesktopBase]"
+                                        )
+        item_list = await self.page.querySelectorAll("div[data-component=CustomDiscoverModules] > "
+                                                     "div:nth-child(2) div[data-component=CardGridDesktopBase]"
+                                                     )
         for index in range(0, len(item_list)):
-            await self.page.waitForSelector("div[data-component=CustomDiscoverModules] > div:nth-child(2) " \
-                "div[data-component=CardGridDesktopBase]"
-            )
-            item = (await self.page.querySelectorAll("div[data-component=CustomDiscoverModules] > " \
-                "div:nth-child(2) div[data-component=CardGridDesktopBase]")
-            )[index]
+            await self.page.waitForSelector("div[data-component=CustomDiscoverModules] > div:nth-child(2) "
+                                            "div[data-component=CardGridDesktopBase]"
+                                            )
+            item = (await self.page.querySelectorAll("div[data-component=CustomDiscoverModules] > "
+                                                     "div:nth-child(2) div[data-component=CardGridDesktopBase]")
+                    )[index]
             await item.click()
             await self.await_try_click("div[class*=WarningLayout__layout] Button")
             if await self.await_try_click("button[data-testid=purchase-cta-button]:not([disabled]):nth-child(1)"):
-                await self.order("#purchase-app > div > div.order-summary-container " \
-                    "> div.order-summary-card > div.order-summary-card-inner " \
-                    "> div.order-summary-content > div > div > button:not([disabled])")        
+                await self.order("#purchase-app > div > div.order-summary-container "
+                                 "> div.order-summary-card > div.order-summary-card-inner "
+                                 "> div.order-summary-content > div > div > button:not([disabled])")
             elif await self.await_try_click("button[data-testid=purchase-cta-button]:not([disabled]):nth-child(2)"):
-                await self.order("#purchase-app > div > div.order-summary-container " \
-                    "> div.order-summary-card > div.order-summary-card-inner " \
-                    "> div.order-summary-content > div > div > button:not([disabled])")
+                await self.order("#purchase-app > div > div.order-summary-container "
+                                 "> div.order-summary-card > div.order-summary-card-inner "
+                                 "> div.order-summary-content > div > div > button:not([disabled])")
             await self.page.goto("https://www.epicgames.com/store/zh-CN/free-games",
-                options={"timeout": 120000}
-            )
+                                 options={"timeout": 120000}
+                                 )
 
 
 if __name__ == "__main__":
@@ -128,7 +129,7 @@ if __name__ == "__main__":
             claimer.log("Claim start.")
             loop.run_until_complete(claimer.claim())
         except Exception as e:
-            claimer.log("{}: {}".format( e.__class__.__name__, e))
+            claimer.log("{}: {}".format(e.__class__.__name__, e))
         finally:
             claimer.close()
             claimer.log("Claim end.")
