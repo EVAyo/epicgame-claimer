9ad35daff43968e6530e173ef9833f38b00c716f
luminoleon <luminoleon@outlook.com>
2021-08-11 10:45:33 +0800

    Optimize

--- epicgames_claimer.py
+++ epicgames_claimer.py
@@ -210,7 +210,7 @@ class epicgames_claimer:
         if password == None or password == "":
             raise ValueError("Password can't be null.")
         if await self._need_login_async():
-            await self._navigate_async("https://www.epicgames.com/store/en-US/", timeout=120000)
+            await self._navigate_async("https://www.epicgames.com/store/en-US/", timeout=120000, reload=False)
             await self._click_async("#user", timeout=120000)
             await self._click_async("#login-with-epic", timeout=120000)
             await self._type_async("#email", email)
